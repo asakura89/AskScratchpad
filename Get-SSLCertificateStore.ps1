a4ed1720e2fec3aefb0c0d940416d75f77cff2a6
asakura89 <flamboyan3no1a@gmail.com>
2019-10-04 14:31:37 +0700

    [.gitignore] Add
    [IIS] IIS and performance related

--- Get-SSLCertificateStore.ps1
+++ Get-SSLCertificateStore.ps1
@@ -1,5 +1,12 @@
 Clear-Host
 
+Write-Host "Certificate Store Location"
+[Enum]::GetValues([Security.Cryptography.X509Certificates.StoreLocation])
+
+Write-Host "Certificate Store Name"
+[Enum]::GetValues([Security.Cryptography.X509Certificates.StoreName])
+
+
 # Personal
 Get-ChildItem Cert:\LocalMachine\My -Recurse
 Get-ChildItem Microsoft.PowerShell.Security\Certificate::LocalMachine\My -Recurse
