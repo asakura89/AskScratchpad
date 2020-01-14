a1d85fe2f001c6140bcda863e96cc4dfe1354f6a
asakura89 <flamboyan3no1a@gmail.com>
2020-01-14 15:56:21 +0700

    [Script] .

--- CSScratchpad/Script/PrintUri.cs
+++ CSScratchpad/Script/PrintUri.cs
@@ -1,19 +1,88 @@
 using System;
+using System.Collections.Specialized;
+using System.Web;
+using System.Web.Security.AntiXss;
 using Scratch;
 
 namespace CSScratchpad.Script {
     public class PrintUri : Common, IRunnable {
         public void Run() {
-            String address = "http://localhost:8090/Services/Employee?dtFromString=20170801&dtToString=20170830";
+            String address = "http://localhost:8090/Services/Employee?dtFromString=20170801&dtToString=20170830&name=john hashkell&jobs=towns+mayor";
             var uri = new Uri(address);
 
-            Dbg("All", uri);
-            Dbg("partial authority", uri.GetLeftPart(UriPartial.Authority));
-            Dbg("partial path", uri.GetLeftPart(UriPartial.Path));
-            Dbg("partial query", uri.GetLeftPart(UriPartial.Query));
-            Dbg("partial scheme", uri.GetLeftPart(UriPartial.Scheme));
-            Dbg("for HttpClient", uri.GetLeftPart(UriPartial.Authority));
-            Dbg("for GetAsync", uri.AbsolutePath);
+            Dbg(new {
+                AllUriProps = uri,
+                PartialAuthority = uri.GetLeftPart(UriPartial.Authority),
+                PartialPath = uri.GetLeftPart(UriPartial.Path),
+                PartialQuery = uri.GetLeftPart(UriPartial.Query),
+                PartialScheme = uri.GetLeftPart(UriPartial.Scheme),
+                ForHttpClient = uri.GetLeftPart(UriPartial.Authority),
+                ForGetAsync = uri.AbsolutePath,
+                NotAllUriProps = new {
+                    uri.Host,
+                    uri.AbsolutePath,
+                    uri.AbsoluteUri,
+                    uri.Authority,
+                    uri.DnsSafeHost,
+                    uri.Port,
+                    uri.Fragment,
+                    uri.HostNameType,
+                    uri.IdnHost,
+                    uri.IsAbsoluteUri,
+                    uri.IsDefaultPort,
+                    uri.IsFile,
+                    uri.IsLoopback,
+                    uri.IsUnc,
+                    uri.LocalPath,
+                    uri.OriginalString,
+                    uri.PathAndQuery,
+                    uri.Query,
+                    uri.Scheme,
+                    uri.Segments,
+                    uri.UserEscaped,
+                    uri.UserInfo
+                }
+            });
+
+            var uriB = new UriBuilder(address);
+            NameValueCollection query = HttpUtility.ParseQueryString(uriB.Query);
+            query["dtFromString"] = HttpUtility.UrlEncode(query["dtFromString"]);
+            query["dtToString"] = HttpUtility.UrlEncode(query["dtToString"]);
+            query["name"] = HttpUtility.UrlEncode(query["name"]);
+            query["jobs"] = HttpUtility.UrlEncode(query["jobs"]);
+            uriB.Query = query.ToString();
+
+            var uriC = new UriBuilder(address);
+            NameValueCollection query2 = HttpUtility.ParseQueryString(uriC.Query);
+            query2["dtFromString"] = HttpUtility.UrlPathEncode(query2["dtFromString"]);
+            query2["dtToString"] = HttpUtility.UrlPathEncode(query2["dtToString"]);
+            query2["name"] = HttpUtility.UrlPathEncode(query2["name"]);
+            query2["jobs"] = HttpUtility.UrlPathEncode(query2["jobs"]);
+            uriC.Query = query2.ToString();
+
+            var uriD = new UriBuilder(address);
+            NameValueCollection query3 = HttpUtility.ParseQueryString(uriD.Query);
+            query3["dtFromString"] = HttpUtility.UrlEncode(query3["dtFromString"]).Replace("+","%20");
+            query3["dtToString"] = HttpUtility.UrlEncode(query3["dtToString"]).Replace("+", "%20");
+            query3["name"] = HttpUtility.UrlEncode(query3["name"]).Replace("+", "%20");
+            query3["jobs"] = HttpUtility.UrlEncode(query3["jobs"]).Replace("+", "%20");
+            uriD.Query = query3.ToString();
+
+            var uriE = new UriBuilder(address);
+            NameValueCollection query4 = HttpUtility.ParseQueryString(uriE.Query);
+            query4["dtFromString"] = AntiXssEncoder.UrlEncode(HttpUtility.UrlDecode(query4["dtFromString"]));
+            query4["dtToString"] = AntiXssEncoder.UrlEncode(HttpUtility.UrlDecode(query4["dtToString"]));
+            query4["name"] = AntiXssEncoder.UrlEncode(HttpUtility.UrlDecode(query4["name"]));
+            query4["jobs"] = AntiXssEncoder.UrlEncode(HttpUtility.UrlDecode(query4["jobs"]));
+            uriE.Query = HttpUtility.UrlDecode(query4.ToString());
+
+            Dbg(new {
+                Original = address,
+                Modified = uriB.ToString(),
+                Modified2 = uriC.ToString(),
+                Modified3 = uriD.ToString(),
+                Modified4 = uriE.ToString()
+            });
         }
     }
 }
--- CSScratchpad/Script/TestMaaya.cs
+++ CSScratchpad/Script/TestMaaya.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Web;
+using System.Web.Security.AntiXss;
 using Scratch;
 
 namespace CSScratchpad.Script {
@@ -24,19 +28,25 @@ namespace CSScratchpad.Script {
                 "../",
                 "../Home.aspx",
                 "Home/Index.aspx",
-                "http:app.localweb.net",
-                "http:/app.localweb.net",
-                "https:app.localweb.net",
-                "https:/app.localweb.net",
-                "http://app.localweb.net/Home/Index",
-                "https://app.localweb.net/Home/Index",
-                "https://google.com"
+                "http:web.development.net",
+                "http:/web.development.net",
+                "https:web.development.net",
+                "https:/web.development.net",
+                "http://web.development.net/Home/Index",
+                "https://web.development.net/Home/Index",
+                "https://google.com",
+                "http://web.development.net/Home/Index?q=hello",
+                "http://web.development.net/Home-Index",
+                "http://web.development.net/Home/Index?q=<script>alert('yeehaw');</script>",
+                "http://web.development.net/Home/Index<en-us>/[page].htm?v={value1}#x=[amount]",
+                "http://web.development.net/Home/Index<en-us>/[page].htm?v={value1}#x=[amount]&q=<script>alert('yeehaw');</script>", // NOTE: fragment issue
+                "http://web.development.net/Home/Index<en-us>/[page].htm?q=<script>alert('yeehaw');</script>&x=a+b&v={value1}#x=[amount]"
             };
 
             Dbg("IsLocalUrl:",
                 urls.Select(url => new {
                     Url = url,
-                    Local = url.IsLocalUrl(new Uri("http://app.localweb.net"))
+                    Local = url.IsLocalUrl(new Uri("http://web.development.net"))
                 })
             );
 
@@ -52,10 +62,10 @@ namespace CSScratchpad.Script {
                 try {
                     new Uri(url);
                 }
-                catch (ArgumentNullException ane) {
+                catch (ArgumentNullException) {
                     return false;
                 }
-                catch (UriFormatException ufe) {
+                catch (UriFormatException) {
                     return false;
                 }
 
@@ -72,12 +82,63 @@ namespace CSScratchpad.Script {
 
             Dbg("Host:",
                 urls
-                    .Skip(urls.Length -3)
+                    .Skip(urls.Length -9)
                     .Select(url => new {
                         Url = url,
-                        Host = new Uri(url).Host,
-                        DnsSafeHost = new Uri(url).DnsSafeHost
+                        new Uri(url).Host,
+                        new Uri(url).DnsSafeHost
                     })
+            );
+
+            IEnumerable<String> toBeCleaneds = urls
+                .Take(3)
+                .Concat(urls
+                    .Skip(urls.Length -9));
+
+            Dbg("CleanedLink:",
+                toBeCleaneds
+                    .Select(url => new {
+                        UriBuilder = new UriBuilder(url),
+                        HtmlEnc = AntiXssEncoder.HtmlEncode(url, true),
+                        UrlEnc = AntiXssEncoder.UrlEncode(url),
+                        CustomEnc = url.AsCleanedLink()
+                    })
+                    .Select(url => new {
+                        Uri = new {
+                            url.UriBuilder.Uri.Host,
+                            url.UriBuilder.Uri.AbsolutePath,
+                            url.UriBuilder.Uri.AbsoluteUri,
+                            url.UriBuilder.Uri.Authority,
+                            url.UriBuilder.Uri.DnsSafeHost,
+                            url.UriBuilder.Uri.Port,
+                            url.UriBuilder.Uri.Fragment,
+                            url.UriBuilder.Uri.HostNameType,
+                            url.UriBuilder.Uri.IdnHost,
+                            url.UriBuilder.Uri.IsAbsoluteUri,
+                            url.UriBuilder.Uri.IsDefaultPort,
+                            url.UriBuilder.Uri.IsFile,
+                            url.UriBuilder.Uri.IsLoopback,
+                            url.UriBuilder.Uri.IsUnc,
+                            url.UriBuilder.Uri.LocalPath,
+                            url.UriBuilder.Uri.OriginalString,
+                            url.UriBuilder.Uri.PathAndQuery,
+                            url.UriBuilder.Uri.Query,
+                            QueryStrings = HttpUtility.ParseQueryString(url.UriBuilder.Uri.Query),
+                            url.UriBuilder.Uri.Scheme,
+                            url.UriBuilder.Uri.Segments,
+                            url.UriBuilder.Uri.UserEscaped,
+                            url.UriBuilder.Uri.UserInfo
+                        },
+                        url.UriBuilder,
+                        url.HtmlEnc,
+                        url.UrlEnc,
+                        url.CustomEnc
+                    })
+            );
+
+            Dbg("CleanedLink:",
+                toBeCleaneds
+                    .Select(url => url.AsCleanedLink())
             );
 
             Console.WriteLine("Done.");
@@ -127,6 +188,67 @@ namespace CSScratchpad.Script {
             }
 
             return false;
+        }
+
+        // NOTE: Return value determine if need to processed further or not
+        static Boolean ParseObject(Object source, out String result) {
+            String cleaned;
+            if (source == null) {
+                result = null;
+                return false;
+            }
+
+            cleaned = source as String;
+            if (cleaned == null) {
+                result = null;
+                return false;
+            }
+
+            cleaned = cleaned.Trim();
+            if (String.IsNullOrEmpty(cleaned)) {
+                result = cleaned;
+                return false;
+            }
+
+            result = cleaned;
+            return true;
+        }
+
+        public static String AsCleanedString(this Object source) {
+            Boolean okForFurtherProcessing = ParseObject(source, out String cleaned);
+            return !okForFurtherProcessing ? cleaned : AntiXssEncoder.HtmlEncode(cleaned, true);
+        }
+
+        public static String AsCleanedLink(this Object source) {
+            Boolean okForFurtherProcessing = ParseObject(source, out String cleaned);
+            if (!okForFurtherProcessing)
+                return cleaned;
+
+            var uriBuilder = new UriBuilder(cleaned);
+            IList<String> cleanedSegments = new List<String>();
+            foreach (String segment in uriBuilder.Uri.Segments) {
+                String cleanedSegment = segment.Replace("/", String.Empty);
+                if (cleanedSegment == String.Empty)
+                    continue;
+
+                cleanedSegments.Add(AntiXssEncoder.UrlEncode(HttpUtility.UrlDecode(cleanedSegment)));
+                // NOTE: we're back and forth because we can't turn off the UriBuilder's default url encoder
+                // and the UriBuilder's default url encoder did not escape singiequote and normal brackets
+            }
+            uriBuilder.Path = String.Join("/", cleanedSegments);
+
+            NameValueCollection queryStrings = HttpUtility.ParseQueryString(uriBuilder.Query);
+            NameValueCollection shadowQueryStrings = HttpUtility.ParseQueryString(uriBuilder.Query);
+            foreach (String key in shadowQueryStrings)
+                queryStrings[key] = AntiXssEncoder.UrlEncode(HttpUtility.UrlDecode(shadowQueryStrings[key]));
+            uriBuilder.Query = HttpUtility.UrlDecode(queryStrings.ToString());
+            uriBuilder.Fragment = AntiXssEncoder.UrlEncode(HttpUtility.UrlDecode(uriBuilder.Fragment).Replace("#", String.Empty));
+
+            // NOTE: hack to remove port
+            if (uriBuilder.Uri.IsDefaultPort)
+                uriBuilder.Port = -1;
+
+            return uriBuilder.Uri.AbsoluteUri;
         }
     }
 }
