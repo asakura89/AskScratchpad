101552938c16016d2ea25979aa4e67922b9c61bf
asakura89 <flamboyan3no1a@gmail.com>
2020-03-06 00:39:59 +0700

    [Array] .

---
+++ Concatenate-Array.ps1
@@ -0,0 +1,12 @@
+Clear-Host
+
+[System.Collections.Generic.List[System.Int32]]$range = 1..10
+[System.Collections.Generic.List[System.Int32]]$range2 = 11..20
+
+$range3 = $range + $range2
+$range3
+
+[System.Collections.Generic.List[System.Int32]]$range4 = 45..65 + 78..98
+
+$range4
+
