266c76653e00a1c16d9af931066046e6c0bad3bc
asakura89 <flamboyan3no1a@gmail.com>
2020-10-29 23:47:40 +0700

    [Script] .

--- PSScratchpad/Get-FilenameByExtension.ps1
+++ PSScratchpad/Get-FilenameByExtension.ps1
@@ -1,9 +1,21 @@
 Clear-Host
 
+function GetFiles($rootDir, $fileExt) {
+    If (-Not $fileExt.StartsWith(".")) {
+        $fileExt = ".$($fileExt)"
+    }
+
+    $fileExt = "*$($fileExt)"
+
+    $files = Get-ChildItem -Path $rootDir -Include $fileExt -Recurse |
+        Select-Object -ExpandProperty VersionInfo  |
+        Select-Object -ExpandProperty FileName
+
+    Return $files
+}
+
 $dir = "C:\Users\<Username>\.nuget\packages"
 $ext = ".nupkg"
 
-Get-ChildItem -Path $dir -Include "*$($ext)" -Recurse |
-    Select-Object -ExpandProperty VersionInfo  |
-    Select-Object -ExpandProperty FileName |
-    Sort-Object
+GetFiles $dir $ext |
+    Sort-Object -Descending
