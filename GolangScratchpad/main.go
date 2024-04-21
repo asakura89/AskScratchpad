8492ef4b80646ef42016db65ae2c16d8251755c5
asakura89 <flamboyan3no1a@gmail.com>
2024-04-21 19:20:52 +0700

    .

--- GolangScratchpad/main.go
+++ GolangScratchpad/main.go
@@ -1,11 +1,31 @@
 package main
 
+/*
 import (
     "askscratchpad/golang/ryang"
     "fmt"
 )
 
 func main() {
-    fmt.Println(ryang.Ryandomize(5, 10))
-    fmt.Println(ryang.RyandomizeUpperBound(10))
+    rng := ryang.RyandomNumberGenerator{}
+
+    fmt.Println(rng.Ryandomize(5, 10))
+    fmt.Println(rng.RyandomizeUpperBound(10))
+}
+*/
+
+import (
+    "fmt"
+)
+
+func main() {
+    a := A{}
+    angka := a.SomeFunc()
+    fmt.Println(angka)
+}
+
+type A struct {}
+
+func (a *A) SomeFunc() (int32) {
+    return 46692016
 }
--- GolangScratchpad/ryang/ryandom_number_generator.go
+++ GolangScratchpad/ryang/ryandom_number_generator.go
@@ -7,12 +7,14 @@ import (
 
 const feigenbaum = 46692016
 
-func Ryandomize(lowerBound, upperBound int32) int32 {
+type RyandomNumberGenerator struct {}
+
+func (rng *RyandomNumberGenerator) Ryandomize(lowerBound, upperBound int32) int32 {
     seed := time.Now().UnixNano() % feigenbaum
     rnd := rand.New(rand.NewSource(seed))
     return lowerBound + rnd.Int31n(upperBound-lowerBound)
 }
 
-func RyandomizeUpperBound(upperBound int32) int32 {
-    return Ryandomize(0, upperBound)
+func (rng *RyandomNumberGenerator) RyandomizeUpperBound(upperBound int32) int32 {
+    return rng.Ryandomize(0, upperBound)
 }
